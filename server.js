0b862f85caad528297533659d2f4ca87cb74a23c
Shivendra singh <singhanshul88508@gmail.com>
2026-09-10 21:11:30 +0530

    Add authentication for register and login user

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 connectDB();
 
-app.use(express.json());
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({
