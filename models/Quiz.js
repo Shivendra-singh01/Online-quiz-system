86f8ae1bc56ae3f840966e477c57add9f22635fb
Shivendra singh <singhanshul88508@gmail.com>
2026-09-10 21:34:11 +0530

    Add admin authorization

---
+++ models/Quiz.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const quizSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type: String
+        },
+
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Quiz", quizSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,20 @@ const express = require("express");
 
 const { register, login } = require("../controllers/authController");
 
+const authMiddleware = require("../middleware/authMiddleware");
+
 const router = express.Router();
+
+
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/me", authMiddleware, (req, res) => {
+    res.json({
+        message: "You are authenticated",
+        user: req.user
+    });
+});
 
 module.exports = router;
